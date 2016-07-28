f2acb784f93041635207ae5e7d62c63827f030e0
AhsanAzim <ahsan-azim@live.com>
2016-07-28 00:45:47 -0400

    basic reqs

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import Counter from '../containers/counter.js';
+import Controls from '../containers/controls.js';
 
 // function based "dumb" component with no state
 const Welcome = () => {
   return (
     <div>
       react+webpack+babel+eslint starter
+      <Counter />
+      <Controls />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 // at top
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
@@ -7,9 +10,17 @@ import ReactDOM from 'react-dom';
 
 import './style.scss';
 
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
 // replace ReactDOM.render with:
 // entry point that just renders app
 // could be used for routing at some point
 ReactDOM.render(
-  <Router history={browserHistory} routes={routes} />
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
   , document.getElementById('main'));
