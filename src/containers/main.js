35d609977345df5d55d285e596063c520c4e2205
AhsanAzim <ahsan-azim@live.com>
2016-09-02 18:57:54 -0400

    basic parallax setup + container classes

--- src/containers/main.js
+++ src/containers/main.js
@@ -8,8 +8,72 @@ class Main extends Component {
 
   render() {
     return (
-      <div>
-        {'Dartmouth Cricket Club'}
+      <div className="parallax">
+        <div id="group1" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="groupHeader" className="parallax__group">
+          <div className="parallax__layer parallax__layer--back">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="group2" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="groupHeader" className="parallax__group">
+          <div className="parallax__layer parallax__layer--back">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="group3" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="groupHeader" className="parallax__group">
+          <div className="parallax__layer parallax__layer--back">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="group4" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="groupHeader" className="parallax__group">
+          <div className="parallax__layer parallax__layer--back">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="group5" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="groupHeader" className="parallax__group">
+          <div className="parallax__layer parallax__layer--back">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="group6" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="groupHeader" className="parallax__group">
+          <div className="parallax__layer parallax__layer--back">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
+        <div id="groupLast" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">Base Layer</div>
+          </div>
+        </div>
       </div>
     );
   }
